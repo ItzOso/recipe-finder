ab427367c90d37090466c8af729b1e4cae3b3858
Oscar Ortiz <ortizoscjr@gmail.com>
2024-10-18 10:44:39 -0700

    setting up for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
   }, [favorites]);
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/recipe-app">
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
